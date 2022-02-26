62daf505da6de7ec009f6cfff7c48d54e4a09bc1
LUCADN4 <91006943+LUCADN4@users.noreply.github.com>
2022-02-26 12:03:43 +0100

    Update main.c

--- TPSIT/6TPSIT/main.c
+++ TPSIT/6TPSIT/main.c
@@ -9,7 +9,7 @@ interi (massimo 100), li collochi in un vettore e inverta il vettore
 Si usi i puntatori.
 */
 
-void caricaVett(int v[], int n) {
+void caricaVett(int v[], int n) {//funzione carica del vettore
     int k;
 
     for(k = 0; k < n; k++) {
@@ -19,7 +19,7 @@ void caricaVett(int v[], int n) {
 
 }
 
-void stampaVet(int v[], int n) {
+void stampaVet(int v[], int n) {//funzione stampa del vettore
     int k;
 
     for(k = 0; k < n; k++) {
@@ -27,7 +27,7 @@ void stampaVet(int v[], int n) {
     }
 }
 
-void inverti(int v[], int n) {
+void inverti(int v[], int n) {//funzione per invertire il vettore
     int k, j = n - 1, x;
 
     for(k = 0; k < n/2; k++) {
@@ -35,33 +35,33 @@ void inverti(int v[], int n) {
         *(v+k) = *(v+j-k);
         *(v+j-k) = x;
     }
-    return;
+  
 }
 
-int dimVet() {
+int dimVet() {//funzione per definire la dimensione vettore
     int n;
     do {
         printf("Inserisci il numero di valori del vettore: ");
         scanf("%d", &n);
-    } while(n < 0 || n > DIM);
+    } while(n < 0 || n > DIM);//la dimensione deve essere compresa tra 0 e 100
 
     return n;
 }
 
 int main() {
-    int v[DIM];
+    int v[DIM];//inizializzato vettore con una dimensione fissa
     int n;
 
 
-    n = dimVet();
+    n = dimVet();//dimensione del vettore intero
 
-    caricaVett(v, n);
+    caricaVett(v, n);//caricato vettore
     printf("VETTORE NORMALE: \n");
-    stampaVet(v, n);
+    stampaVet(v, n);//stampa del vettore
 
-    inverti(v, n);
+    inverti(v, n);//vettore invertito
     printf("\nVETTORE INVERTITO: \n");
-    stampaVet(v, n);
+    stampaVet(v, n);//stampa vettore invertito
 
     printf("\n");
     return 0;
